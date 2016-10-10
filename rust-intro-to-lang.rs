aec82f4d1d9b6d87b7c5a983776a73c8473e4401
Sultan Tarimo <sultantarimo@me.com>
2016-10-10 13:51:35 +0200

    ++arrays, intro-to-rust

--- rust-intro-to-lang.rs
+++ rust-intro-to-lang.rs
@@ -9,6 +9,7 @@
 // char,  unicode scalar values like 'a', 'α' and '∞'
 // bool, true or false
 // arrays, [1, 2, 3]
+// vectors, re-sizable arrays
 // tuples, (1, true)
 
  
@@ -23,11 +24,14 @@ let boolean: bool      = true;
 let string: &str       = "String"
 let string             = "String" // ^ same thing
 let character          = 'x'; // uses single tick ` ' `
-let arr: [&str; 3]     = ["String 1", "String 2", "String 3"]; 
-let arr                = ["String 1", "String 2", "String 3"];
+let arr: [&str; 3]     = ["String 1", "String 2", "String 3"]; // fixed size array
+let arr                = ["String 1", "String 2", "String 3"]; // ^ same thing
+let mut arr: Vec<&str> = vec!["String 1", "String 2", "String 3"]; // a dynamic array (vector)
+let mut arr:           = vec!["String 1", "String 2", "String 3"]; // same thing
 let tpl: (i32, &str)   = (1, "hello");
-let tpl                = (1, "hello");
+let tpl                = (1, "hello"); // ^ same thing
 let fc: fn(i32) -> i32 = foo;
+
 
 /**
  * ---------------------------------------------------------------------------------
@@ -161,3 +165,65 @@ for x in 11..0 {
 
 
 // ...to be continued ->
+
+
+/**
+ * ---------------------------------------------------------------------------------
+ * 
+ * @arrays
+ * 
+ * ---------------------------------------------------------------------------------
+ */
+
+
+let ints: [i32; 3]  = [1, 2, 3];
+let strs: [&str; 3] = ["A", "B", "C"];
+let buls: [bool; 3] = [true, false, true];
+let flts: [f64; 3]  = [1.0, 2.0, 3.0];
+
+let length = strs.len();     // 2
+let value  = strs[0];        // "A"
+
+// slices point to a section of an array
+let slice  = &str[1 .. 2];   // ["B", "C"]
+let slice: [&str] = &strs;   // ["A", "B", "C"]
+
+// all of the above arrays are all immutable, below are mutable arrays called vectors
+let mut ints: Vec<&i32>  = vec![1, 2, 3];
+let mut strs: Vec<&str>  = vec!["A", "B", "C"];
+let mut buls: Vec<&bool> = vec![true, false, true];
+let mut flts: Vec<&f64>  = vec![1.0, 2.0, 3.0];
+
+// we could leave the explicit mention of Vec<>
+let mut ints = vec![1, 2, 3];
+let mut strs = vec!["A", "B", "C"];
+let mut buls = vec![true, false, true];
+let mut flts = vec![1.0, 2.0, 3.0];
+
+str.append("D")             // str => ["A", "B", "C", "D"]
+str.insert(1, "F")          // str => ["A", "F", "B", "C", "D"]
+
+let str2: Vec<&str> = vec![4, 5, 6];
+str.append(&mut str2);     // ints => [1, 2, 3, 4, 5, 6]
+
+buls.remove(0);            // buls => [false, true]
+flts[2] = 4.0              // flts => [1.0, 2.0, 4.0]
+
+// trying to access an index that does not exist
+
+let v = vec![1, 2, 3];
+println!("Item 7 is {}", v[7]);
+// then the current thread will panic with a message like this:
+
+// thread 'main' panicked at 'index out of bounds: the len is 3 but the index is 7'
+
+// If you want to handle out-of-bounds errors without panicking, 
+// you can use methods like get or get_mut that return None when given an invalid index:
+
+let v = vec![1, 2, 3];
+match v.get(7) {
+    Some(x) => println!("Item 7 is {}", x),
+    None => println!("Sorry, this vector is too short.")
+}
+
+// ... to be continued ->
